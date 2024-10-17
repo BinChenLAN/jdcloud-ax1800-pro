2244152253a2e46590b942aefab1f2000022d911
ec.mbp <d0o0bz@qq.com>
2024-10-17 14:23:42 +0800

    update

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,10 +11,18 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.15.1/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/OpenWrt-JDC-AX1800-Pro/g' package/base-files/files/bin/config_generate
+
+##-----------------Add dev core for kenzo OpenClash------------------
+curl -sL -m 30 --retry 2 https://raw.github.com/vernesong/OpenClash/core/master/dev/clash-linux-arm64.tar.gz -o /tmp/clash.tar.gz
+tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
+chmod +x /tmp/clash >/dev/null 2>&1
+mkdir -p package/kenzo/luci-app-openclash/root/etc/openclash/core
+mv /tmp/clash package/kenzo/luci-app-openclash/root/etc/openclash/core/clash >/dev/null 2>&1
+rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
